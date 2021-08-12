d8d320ea46e3f405d54e3aa530a32c6abda49ed3
Сергей Полещук <sergv77@yandex.ru>
2021-08-12 10:27:14 +0300

    Обновление зависимостей

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -21,7 +21,6 @@ visualize(models, default_text)
 # text = st.text_area("Текст для анализа", default_text, height=200)
 # doc = spacy_streamlit.process_text(*models, text)
 
-
 #
 # tmp_text = 'Анестезиологические и респираторные медицинские изделия. Ортопедические медицинские изделия.'
 # doc = nlp(tmp_text)
